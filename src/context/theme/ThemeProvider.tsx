b82c4052188f03c865376e60040d40224a395580
Rohit Pakhrin <roitpak@gmail.com>
2025-01-16 10:15:28 +0545

    dark mode light mode to preferences

--- src/context/theme/ThemeProvider.tsx
+++ src/context/theme/ThemeProvider.tsx
@@ -1,15 +1,28 @@
-import React, {FC, PropsWithChildren, useState} from 'react';
+import React, {FC, PropsWithChildren, useEffect, useState} from 'react';
 import {ThemeContext} from './ThemeContext';
 import {DarkTheme, LightTheme} from '../../constants/ThemeValue';
 import {useColorScheme} from 'react-native';
-import {DARK_MODE} from '../../constants/Constants';
+import {DARK_MODE, LIGHT_MODE} from '../../constants/Constants';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const THEME_VALUE = 'themeMode';
 
 export const ThemeProvider: FC<PropsWithChildren> = ({children}) => {
   const isDarkMode = useColorScheme() === DARK_MODE;
   const [darkMode, setDarkMode] = useState(isDarkMode);
 
-  const changeTheme = () => {
+  const changeTheme = async () => {
+    await AsyncStorage.setItem(THEME_VALUE, darkMode ? LIGHT_MODE : DARK_MODE);
     setDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    startUpTheme();
+  });
+
+  const startUpTheme = async () => {
+    const themeValue = await AsyncStorage.getItem(THEME_VALUE);
+    setDarkMode(themeValue === DARK_MODE ? true : false);
   };
 
   const getTheme = () => {
